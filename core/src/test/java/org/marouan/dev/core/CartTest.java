3b7c991a4d2bd060395ec8a72b55b5168e1d607a
marouan.el-hajjaji <marouan.el-hajjaji@happn.fr>
2023-10-25 18:02:03 +0200

    [RED] add TU with two same BTTF parts bought

--- core/src/test/java/org/marouan/dev/core/CartTest.java
+++ core/src/test/java/org/marouan/dev/core/CartTest.java
@@ -3,11 +3,15 @@ package org.marouan.dev.core;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.math.BigDecimal;
-import java.util.Collections;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class CartTest {
+
+  private final static List<Discount> discounts = List.of(
+      new Discount("Back to the Future", 2, BigDecimal.valueOf(0.1)),
+      new Discount("Back to the Future", 3, BigDecimal.valueOf(0.2))
+  );
 
   @Test
   void calculatePrice_shouldReturnCartTotalPrice_whenMoviesInCart() {
@@ -29,10 +33,6 @@ class CartTest {
   @Test
   void calculatePrice_shouldReturnCartTotalPriceAndApplyDiscount_whenTwoBTTFPartsBought() {
     // GIVEN
-    final List<Discount> discounts = List.of(
-        new Discount("Back to the Future", 2, BigDecimal.valueOf(0.1)),
-        new Discount("Back to the Future", 3, BigDecimal.valueOf(0.2))
-    );
     final List<Movie> movies = List.of(
         new Movie("Back to the Future", 1, new BigDecimal(15)),
         new Movie("Back to the Future", 2, new BigDecimal(15)),
@@ -46,5 +46,23 @@ class CartTest {
 
     // THEN
     assertEquals(0, price.compareTo(new BigDecimal(47)));
+  }
+
+  @Test
+  void calculatePrice_shouldReturnCartTotalPriceAndNotApplyDiscount_whenTwoBTTFSamePartsBought() {
+    // GIVEN
+    final List<Movie> movies = List.of(
+        new Movie("Back to the Future", 1, new BigDecimal(15)),
+        new Movie("Back to the Future", 1, new BigDecimal(15)),
+        new Movie("Interstellar", 1, new BigDecimal(20))
+    );
+    final Cart cart = new Cart(movies);
+    final DiscountedCart discountedCart = new DiscountedCart(cart, discounts);
+
+    // WHEN
+    final BigDecimal price = discountedCart.calculatePrice();
+
+    // THEN
+    assertEquals(0, price.compareTo(new BigDecimal(50)));
   }
 }
