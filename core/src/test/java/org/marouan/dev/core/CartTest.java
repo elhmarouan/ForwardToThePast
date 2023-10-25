a0a8ad2bbe49f3fcca85fe9d9ded81e2ce73c8a4
marouan.el-hajjaji <marouan.el-hajjaji@happn.fr>
2023-10-25 18:21:32 +0200

    [GREEN] add statement use cases in unit tests

--- core/src/test/java/org/marouan/dev/core/CartTest.java
+++ core/src/test/java/org/marouan/dev/core/CartTest.java
@@ -4,7 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class CartTest {
 
@@ -64,5 +68,35 @@ class CartTest {
 
     // THEN
     assertEquals(0, price.compareTo(new BigDecimal(50)));
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideMoviesUseCases")
+  void calculatePrice_shouldReturnCartDiscountedPrice_whenValidCartForDifferentUseCases(final List<Movie> movies, final BigDecimal price) {
+    // GIVEN
+    final Cart cart = new Cart(movies);
+    final DiscountedCart discountedCart = new DiscountedCart(cart, discounts);
+
+    // WHEN
+    final BigDecimal calculatedPrice = discountedCart.calculatePrice();
+
+    // THEN
+    assertEquals(0, calculatedPrice.compareTo(price));
+  }
+
+
+  private static Stream<Arguments> provideMoviesUseCases() {
+    final Movie bTTF1 = new Movie("Back to the Future", 1, new BigDecimal(15));
+    final Movie bTTF2 = new Movie("Back to the Future", 2, new BigDecimal(15));
+    final Movie bTTF3 = new Movie("Back to the Future", 3, new BigDecimal(15));
+    final Movie laChevre = new Movie("La chèvre", 1, new BigDecimal(20));
+
+    return Stream.of(
+        Arguments.of(List.of(bTTF1, bTTF2, bTTF3), new BigDecimal(36)),
+        Arguments.of(List.of(bTTF1, bTTF3), new BigDecimal(27)),
+        Arguments.of(List.of(bTTF1), new BigDecimal(15)),
+        Arguments.of(List.of(bTTF1, bTTF2, bTTF3, bTTF2), new BigDecimal(48)),
+        Arguments.of(List.of(bTTF1, bTTF2, bTTF3, laChevre), new BigDecimal(56))
+    );
   }
 }
